f26873a4a69ab21aa1b3a210d6e30fbc48242b55
taranshhumai <taransh13@gmail.com>
2026-09-07 15:19:39 +0530

    Minimal Flask app - clean slate

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 import os
 app = Flask(__name__)
-@app.route('/')
+@app.route("/")
 def hello():
     return "ONYX is alive!"
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
--- test.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-import os
-app = Flask(__name__)
-@app.route('/')
-def hello():
-    return "ONYX is alive!"
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
