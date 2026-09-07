736506b3e20625cf941a3c4d113ef61526cb3f62
taranshhumai <taransh13@gmail.com>
2026-09-07 14:47:09 +0530

    test minimal

---
+++ test.py
@@ -0,0 +1,8 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return "ONYX is alive!"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
